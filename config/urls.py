bb71cb78b925652b15858a21516fe4845edcd205
1f10220123 <s1f102201234@iniad.org>
2023-07-06 14:18:27 +0900

    コードの整理

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.urls import path
 from todo import views as todo_views
 
+
 urlpatterns = [
-    path('admin/', admin.site.urls) ,  
+    path('admin/' , admin.site.urls ) ,  
     path("" , todo_views.index , name = "index" ) , 
 ]
